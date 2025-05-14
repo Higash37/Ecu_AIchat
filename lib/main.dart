3093ebd451ef58ade66ae563eb7562883c9244ef
Higash37 <higashionna37@icloud.com>
2025-05-14 13:47:23 +0900

    'supabase連携途中'

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'chat_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // .envの読み込み（SUPABASE_URLとKEYを使うため）
+  await dotenv.load(fileName: ".env");
+
+  // Supabase初期化
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
   runApp(const MyApp());
 }
 
