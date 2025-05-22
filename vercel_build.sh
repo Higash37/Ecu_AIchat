7c97d85073fba09b6cc4d55c0be5b6f97c42ed55
Higash37 <higashionna37@icloud.com>
2025-05-23 06:26:44 +0900

    'sh3'

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Flutter install & build for Vercel
+set -e
 curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.19.6-stable.tar.xz
 tar xf flutter_linux_3.19.6-stable.tar.xz
 export PATH="$PATH:$(pwd)/flutter/bin"
